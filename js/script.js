877ce5fd5c483254d27dea78527b46e7ec5f5295
estydaven <esetnod1111@gmail.com>
2022-07-01 16:28:37 +0300

    add date

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-// Time
+// Time and Date
 const time = document.querySelector('.time');
+const calendar = document.querySelector('.date');
 
 function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime; 
     setTimeout(showTime, 1000);
+    showDate();
 }
 
 showTime();
+
+function showDate() {
+    const date = new Date();
+    const options = {month: 'long', day: 'numeric'};
+    const currentDate = date.toLocaleDateString('en-EN', options);
+    calendar.textContent = currentDate;
+}
